9764843146b51ec26ca2a70277112d14b85f291f
marcelawasum <marcel.awasum@gmail.com>
2018-12-20 04:26:02 +0100

    extending code to wite to CSV file

--- sentimentTweet.py
+++ sentimentTweet.py
@@ -1,4 +1,5 @@
 import tweepy
+import csv
 from textblob import TextBlob
 
 
@@ -8,6 +9,9 @@ consumer_secret = 'fq9VINTa2Jnjs4JB3hfWB5FESvVbDWEuoGWgyR4Gwbwai7JrlZ'
 access_token = '3024123880-aoYgpvlxRK4BadEbPDZCbU10Hn4g6fAEG2Tuf3F'
 access_token_secret = 'WaGo9FNmyg72y9XXqbhmMdc1vrzrYw0Rw5GPrdNpNDRBA'
 
+polarity_threshold = 5
+csv_file="tweet_results.csv"
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
@@ -15,7 +19,27 @@ api = tweepy.API(auth)
 
 public_tweets = api.search('Trump')
 
-for tweet in public_tweets:
-	print(tweet.text)
-	analysis = TextBlob(tweet.text)
-	print(analysis.sentiment)
+file = open(csv_file, "a")
+
+list_polarity = []
+list_test = []
+
+with file:
+	writer = csv.writer(file)
+
+	for tweet in public_tweets:
+		print(tweet.text)
+		analysis = TextBlob(tweet.text)
+		list_test.append(tweet.text)
+
+		if analysis.sentiment.polarity > 5:
+
+			#list_polarity.append("positive")
+			label= "positve"
+		else:
+			label="negetive"
+			#list_polarity.append("negetive")
+		writer.writerow([tweet.text,label])
+
+file.close()
+
